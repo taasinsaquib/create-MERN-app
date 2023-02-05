c4c1328dbab89ff9e5f6ed79509b3e3bc7dce38a
Taasin <taasin.saquib@gmail.com>
2023-02-05 13:47:52 -0800

    env var support

--- api/dist/index.js
+++ api/dist/index.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
+;
 app.get('/', (req, res) => {
+    console.log(process.env.FOO);
     res.send('Hello, this is Express + TypeScript');
 });
 app.listen(port, () => {
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,12 @@
 import express, {Express, Request, Response} from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
 app.get('/', (req: Request, res: Response)=>{
-    res.send('Hello, this is Express + TypeScript');
+    res.send(`Hello, this is Express + TypeScript ${process.env.FOO}`);
 });
 
 app.listen(port, ()=> {
-    console.log(`[Server]: I am running at https://localhost:${port}`);
+    console.log(`[Server]: I am running at http://localhost:${port}`);
 });
